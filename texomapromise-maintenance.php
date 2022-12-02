28eab262c56625664bf306f8d9c4244624e87386
lyszczarzj <lyzczarzj@grayson.edu>
2022-12-02 13:50:24 -0600

    Added pages for texoma promise

--- texomapromise-maintenance.php
+++ texomapromise-maintenance.php
@@ -8,9 +8,9 @@ header('Retry-After: 3600') // Try again in one hour?
     <head>
         <meta charset="utf-8"/>
         <meta content="default-src 'none'; style-src 'self' https://fonts.googleapis.com; media-src 'self'; img-src 'self'; font-src 'self' *.fontawesome.com fontawesome.com https://fonts.gstatic.com; script-src 'self' *.fontawesome.com fontawesome.com https://fonts.googleapis.com;" http-equiv="Content-Security-Policy"/>
-        <title>Under Maintenance | Grayson College</title>
+        <title>Under Maintenance | Texoma Promise</title>
         <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
-        <meta content="Grayson College" name="author"/>
+        <meta content="Texoma Promise" name="author"/>
         <link href="https://fonts.gstatic.com" rel="preconnect"/>
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&amp;display=swap" rel="stylesheet"/>
         <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,700;1,100;1,200;1,300;1,400;1,700&amp;display=swap" rel="stylesheet"/>
@@ -20,12 +20,12 @@ header('Retry-After: 3600') // Try again in one hour?
     <body>
         <main>
             <div class="container">
-            <img class="logo" width="503" height="275" alt="Grayson College" src="temp-files/grayson-web-logo-5.png"/>
+            <img class="logo" width="500" height="175" alt="Texoma Promise Pledge" src="temp-files/texomapromise-logo-web.jpg"/>
             <h1>Under Maintenance</h1>
             <hr/>
-            <p>The Grayson College website is temporarily down for maintenance. <br/>Please check back soon!</p>
-            <p>MyViking Portal and Canvas are still accessible at this time.</p>
-            <a class="btn" title="Access GC Identity Portal" href="https://gcidentity.grayson.edu/">MyViking / GCIdentity</a>
+            <p>The Texoma Promise website is temporarily down for maintenance. <br/>Please check back soon!</p>
+            <p>The Promise Pledge form is still available</p>
+            <a class="btn" title="Texoma Promise Pledge Form" href="https://grayson.tfaforms.net/f/texomapromise">Complete the Promise Pledge</a>
             </div>
         </main>
     </body>
